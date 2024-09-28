7860112b2940d77b1c82858ce2fdb2408bea760b
Masudur400 <md8004210@gmail.com>
2024-09-28 22:55:48 +0600

    file setup complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+       
+      <h1 className='text-2xl font-bold'>Filter By Category</h1>
+       
+       
+    </>
+  )
+}
+
+export default App
