e3ce22f5a90d373bd8f64b62d1fa950fa58244cc
Masudur400 <md8004210@gmail.com>
2024-09-29 19:26:02 +0600

    margin bottom added for outlate

--- src/component/Root/Root.jsx
+++ src/component/Root/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div className='container mx-auto px-2'>
+        <div className='container mx-auto px-2 mb-16'>
             <Outlet></Outlet>
         </div>
     );
